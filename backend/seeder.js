c934e1504a5b4637ffeb618d96ce3a2ffb8f5dee
dantaayy <dantaayy@gmail.com>
2022-07-18 13:43:23 -0700

    updates to seeder

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,37 +1,34 @@
 //import dependencies
 const path = require('path'); //helps us find our file easily
-const fs = require('fs').promises; //helps us get access to promises when dealing with seeding data into our database
+const fs = require('fs').promises; //helps us get access to promises
+const { sneakers } = require('./src/data/sneakers')
 
 //import our database [x]
-const { db } = require('./src/db');
+const sequelize = require('./src/db');
 //import the model that we are trying to import our data into [x]
 const Shoe = require('./src/models/Shoe'); //Check the file path of this model
 
+const debug = require('debug')('app:seeder');
+const colors = require('colors');
+
 //write our seed function -> take our json file, create rows with our data into it
-const seed = async () => {
-  await db.sync({ force: true }); // clear out database + tables
-
-  const shoeSeedPath = path.join(
-    __dirname,
-    backend,
-    src,
-    data,
-    'sneakers.js'
-  ); //gets the path to userData.json
-
-  //asynchronously reads the content in this file
-  const shoeBuffer = await fs.readFile(shoeSeedPath);
-
-  // First we convert the data from buffer into a string, then we parse the JSON so it converts from string -> object
-  const { usersData: sneakersData } = JSON.parse(String(shoeBuffer));
-
-  //creates Show and puts it into our Show table
-  const ShoePromises = sneakersData.map((shoe) => User.create(shoe));
-
-  // The Promise.all() method takes an iterable of promises as an input, and returns a single Promise that resolves to an array of the results of the input promises.
-  await Promise.all(ShoePromises);
-
-  console.log('User database info populated!');
+const populateDb = async () => {
+  debug('Pupulating DB...');
+  try {
+	  // reset the DB
+    await sequelize.sync({force: true});
+    //POPULATE DB
+    await Shoe.bulkCreate(sneakers);
+    debug(colors.green.inverse("SUCCESS: Database has been re-populated!"));
+    process.exit(1)
+  } catch (error) {
+    debug(color.red(`Error with seeding database: ${error.message}`));
+    process.exit(1);
+  }
 };
 
-seed();
+if (process.argv[2] === '-import') {
+  populateDb();
+} else {
+  debug(colors.yellow('No changes to database'));
+}
