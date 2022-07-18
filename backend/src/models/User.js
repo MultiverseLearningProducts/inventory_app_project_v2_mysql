02c7284085aeb24ac18bee4bf2a3784bb0ec5309
joannembl <jmbliberato@gmail.com>
2022-07-18 14:09:30 -0700

    set up sequelize User.js

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,6 +1,16 @@
 const sequelize = require('../db');
 const { Sequelize } = require('sequelize');
 
-const User = sequelize.define('user', {}); //Complete this!
+const User = sequelize.define('user', {
+    first_name: Sequelize.STRING,
+    last_name: Sequelize.STRING,
+    email: Sequelize.STRING,
+    password: Sequelize.STRING,
+    job_title: Sequelize.STRING,
+    profile_pic: Sequelize.STRING,
+    location: Sequelize.STRING,
+    avatar: Sequelize.STRING,
+    projects: Sequelize.STRING
+}); 
 
 module.exports = User;
