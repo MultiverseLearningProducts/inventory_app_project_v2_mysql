d1fbd108294d0fece33699e4c23a26f4cf9d5c10
darrellrayford <darrellrayford@gmail.com>
2022-07-19 17:57:19 -0500

    Nav added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import { useState, useEffect } from 'react';
-import './App.css';
+// import './App.css';
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className='App'>
+      <Nav />
       <h1>Inventory App</h1>
     </div>
   );
---
+++ frontend/src/Components/Nav.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Nav() {
+  return (
+    <nav class="topnav">
+    <a class="active" href="#Inventory-App">Inventory App</a>
+    <a href="#about">About</a>
+    <a href="#contact">Contact</a>
+    <a href="#Users-Page">Users Page</a>
+    <a href="#User-Page">User Page</a>
+    <a href="#Products-Page">Products Page</a>
+    <a href="#Product-Page">Product Page</a>
+    <a href="#User-Form">User Form</a>
+    <a href="#Product-Form">Product Form</a>
+  </nav>
+  )
+}
+
+export default Nav
