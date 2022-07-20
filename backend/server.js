274ad511ff446db1ba4808a26e3d673e1f325883
Roohanjyot <roohansidhu@gmail.com>
2022-07-20 13:28:09 -0700

    debugging database connecting to server

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,27 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
-const debug = require('debug');
-
+const debug = require('debug')('app:server');
+const morgan = require('morgan');
 const sequelize = require('./src/db');
 const colors = require('colors');
 
 const app = express();
-const routes = require('./src/routes/index.js')
+// const routes = require('./src/routes/index.js')
+
+app.use(morgan('dev'));
 app.use(cors()); //allows for cross-origin resource sharing
-
-//app.get('/', (req, res) => res.send('Home'))
-
-app.use('/', routes);
-const server = app.listen(8000, () => {
-  console.log(`Server is up and running on PORT: 8000`);
-});
-
 
 //Authenticate/Test mySQL connection
 sequelize
   .authenticate()
   .then((res) => debug(colors.rainbow.inverse('Database is connected')))
-  .catch((err) => {debug(colors.red.inverse('There was an error connecting to the Database'), err);
-  process.exit(1); //Terminates node servers
+  .catch((err) => {
+    debug(console.error(`There was an error connecting to the Database`.red));
+    process.exit(1); //Terminates node servers
+  });
+
+// app.use('/', routes);
+const server = app.listen(8000, () => {
+  console.log(`Server is up and running on PORT: 8000`.green);
 });
-
-
-//Routes
-app.use('/api', routes);
-
-
