d97f9aaedfdd7507795042735e3a98891626823e
gkutieva <gvk4@nau.edu>
2022-07-20 10:16:15 -0700

    some css

--- frontend/src/pages/CreateUser.jsx
+++ frontend/src/pages/CreateUser.jsx
@@ -2,10 +2,11 @@ import React from 'react';
 import LinkButton from '../components/LinkButton';
 import UserForm from '../components/UserForm';
 
+
 function CreateUser() {
   return (
     <div className='container' style={{flexDirection: 'column'}}>
-        <h1>Create User: </h1>
+        <p>Please fill out this form to create a new user</p>
         <UserForm />
         <LinkButton to='/users' text={'Back to Users'}/>
     </div>
