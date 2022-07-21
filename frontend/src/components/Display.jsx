cc4aa1c9af9cd59b3c7e16e3fd61dfb101f608cf
Alexander Isaias Pacheco <apachofficial@gmail.com>
2022-07-21 10:41:23 -0700

    saving my button

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import "./photos.css"
+import "./photos.css";
+import popUp from '../pages/Product';
 
 function ProductCard({cats}) {
     return (
         <div className='cards'>
             <h3>Cat:  </h3>
             <h2>{cats.name} </h2>
-            <h4>{cats.description} </h4>
             <img className = "catPhotos" src={cats.image_url} alt={cats.name}></img>
+            <button onClick={() => <popUp/>}>Details</button>
         </div>
     )
 
