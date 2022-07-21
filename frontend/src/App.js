715a7a6416d18829700c81f5fbe5a4c8f16afb03
Alexander Isaias Pacheco <apachofficial@gmail.com>
2022-07-21 14:56:10 -0700

    working on getting data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,22 @@
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import ProductS from './pages/Products';
 import './index.css'
+import Product from './pages/Product';
+
+
 
 function App() {
+
+
 
   return (
     <Router>
       <Routes>
         <Route path='/' element={<ProductS />} />
+        <Route path='/:id' element={<Product />} />
       </Routes>
     </Router>
+
   );
 }
 
