f971b09230a3adedec86d7f7e565585279294e5b
Roohanjyot <roohansidhu@gmail.com>
2022-07-21 10:22:10 -0700

    implemented code refactoring

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,36 +1,15 @@
-import { useState, useEffect } from 'react';
-import DisplayList from './components/DisplayList';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import ProductS from './pages/Products';
 import './index.css'
 
 function App() {
-  const [cats, setCats] = useState([]);
-
-  const getData = async () => {
-    const response = await fetch('http://localhost:8000/cats');
-    const data = await response.json();
-
-    console.log('Data: ', data);
-    const { cats } = data;
-    setCats(cats);
-    console.log('Cats: ', cats);
-  }
-
-  useEffect(() => {
-    getData();
-  }, []);
-
-
 
   return (
-    <div className='App'>
-      <div className='header'>
-        <h1>Inventory App</h1>
-
-      </div>
-      <div className='cats'>
-      {cats.length > 0 ? <DisplayList displayList ={cats}/> : 'No cats to display'}
-      </div>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<ProductS />} />
+      </Routes>
+    </Router>
   );
 }
 
